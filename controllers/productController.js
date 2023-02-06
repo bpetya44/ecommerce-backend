a1c11be8fd86b892dc56bcc12f781ee3a522a2a0
Petya Angelova <bedfordpetya@gmail.com>
2023-02-06 20:17:08 +0200

    sorting, pagination, field limiting

--- controllers/productController.js
+++ controllers/productController.js
@@ -57,6 +57,7 @@ const getProduct = asyncHandler(async (req, res) => {
 const getAllProducts = asyncHandler(async (req, res) => {
   //console.log(req.query);
   try {
+    //Filtering
     // const products = await Product.find({
     //     brand: req.query.brand,
     //     category: req.query.category,
@@ -70,7 +71,36 @@ const getAllProducts = asyncHandler(async (req, res) => {
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     //console.log(JSON.parse(queryStr));
 
-    const query = Product.find(JSON.parse(queryStr));
+    let query = Product.find(JSON.parse(queryStr));
+
+    //Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    //Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    //Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 10;
+    const skip = (page - 1) * limit;
+    console.log(page, limit, skip);
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numProducts = await Product.countDocuments();
+      if (skip >= numProducts) throw new Error("This page does not exist");
+    }
+
     const products = await query;
     res.json(products);
   } catch (error) {
